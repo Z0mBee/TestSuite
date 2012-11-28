3deca0e5c27a6230b14c47a05cc5051f0b4a4404
beeswax <user@server>
2012-11-28 18:43:35 +0100

    Miscellaneous changes: - Use python 2.7 if available - git ignore *.pyc

--- generate-gen-py.bat
+++ generate-gen-py.bat
@@ -1,2 +1,14 @@
-C:\Python26\Lib\site-packages\PyQt4\uic\pyuic.py gui.ui > gen.py
+setlocal
+if "%_PYTHON_DIR%" == "" (
+    if exist c:\Python27 (
+        set _PYTHON_DIR=c:\Python27
+    ) else if exist c:\Python26 (
+        set _PYTHON_DIR=c:\Python26
+    ) else (
+        echo Error: Python not found!
+        goto :eof
+    )
+)
+    
+%_PYTHON_DIR%\Lib\site-packages\PyQt4\uic\pyuic.py gui.ui > gen.py
 pause
